6c1b4fc0933064960f6ca7759a29839c9f27f6f5
Chris <christian.bellei@sptech.school>
2023-10-05 20:14:40 -0300

    Exercicios de Kotlin

---
+++ IDE/projeto-filmes/src/main/kotlin/AppFilme.kt
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane
+
+fun main(){
+    Conexao.criarTabelas()
+    val repositorio = Repositorio()
+    repositorio.iniciar()
+    while(true){
+        val filme:Int = JOptionPane.showInputDialog("Escolha uma das alternativas abaixo:/n 1 - Adicionar Filme/n 2 - Consultar Filme/n 3 - Exluir filme/n 4 - Sair").toInt()
+if(filme == 1) {
+    val novoFilme = repositorio()
+    novoFilme.nome = JOptionPane.showInputDialog("Qual o nome do filme?").toString()
+    novoFilme.diretor = JOptionPane.showInputDialog("Qual é o diretor do filme?").toString()
+    novoFilme.indicadoOscar = JOptionPane.showInputDialog("Foi indicado ao Oscar?").toBoolean()
+    novoFilme.custoProducao = JOptionPane.showInputDialog("Quanto foi o custo de produção?").toDouble()
+
+}
+        if(filme == 2){
+            Conexao.Filme(id)
+        }
+        if(filme == 3){
+"?"
+    }
+        break
+}
+}
---
+++ IDE/projeto-filmes/src/main/kotlin/Repositorio.kt
@@ -0,0 +1,20 @@
+import org.springframework.jdbc.core.JdbcTemplate
+
+class Repositorio {
+    lateinit var JdbcTemplate: JdbcTemplate
+
+    fun iniciar(){
+        /* aqui usamos !! porque está com ? na Conexão
+        isso significa que aceitamos que pode ocorrer um erro
+        já que o jdbcTemplate pode estar nulo (null)
+         */
+        JdbcTemplate = Conexao.JdbcTemplate!!
+    }
+    fun cadastrar(Conexao: Filme){
+        JdbcTemplate.update("""
+            insert into pizza (nome, diretor, indicadoOscar, custoProducao) values 
+('${Filme.nome}', ${Filme.diretor}, ${Filme.indicadoOscar}, ${Filme.custoProducao})
+""")
+
+    }
+}
